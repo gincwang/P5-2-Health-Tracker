a5ec58fbda068d8a5f5a587262195592050aec11
gincwang <gincwang@gmail.com>
2015-10-28 14:23:40 -0700

    change color scheme and add + button for food

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -28,8 +28,10 @@ var app = app || {};
             if(input){
                 var appID = "db85c300";
                 var appKey = "58eea2e9289fa4e28d86c6baa320e03f";
-                var url = "https://api.nutritionix.com/v1_1/search/" + input + "?results=0:10&fields=item_name,brand_name,item_id,nf_calories&appId=" + appID + "&appKey=" + appKey;
-                //var collection = this.foodListView.collection;
+                var url = "https://api.nutritionix.com/v1_1/search/" + input + "?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=" + appID + "&appKey=" + appKey;
+                //reset the collection first before ajax request
+                this.foodList.reset();
+                this.foodListView.render();
 
                 $.ajax({
                     context: this,
@@ -39,7 +41,6 @@ var app = app || {};
                     .done(function(response){
                     if(response.hits.length > 0){
                         //clear current collection
-                        this.foodList.reset();
                         console.log(this.foodList);
                         _.each(response.hits, function(element, index, list){
                             console.log(element.fields);
--- js/views/food-view.js
+++ js/views/food-view.js
@@ -10,7 +10,7 @@ var app = app || {};
     app.FoodView = Backbone.View.extend({
         tagName: "li",
         className: "food-view",
-        template: "<div class='item-descriptions'><h4 class='item-name'>{{ item_name }}</h4><h5 class='brand-name'>{{ brand_name }} </h5></div><div class='nutrition'><span class='calorie'>{{ nf_calories }} kcal</span></div>",
+        template: "<div class='item-descriptions'><h4 class='item-name'>{{ item_name }}</h4><h5 class='brand-name'>{{ brand_name }} </h5></div><div class='nutrition'><span class='calorie'>{{ nf_calories }} kcal </span><button type='button'> + </button></div>",
         render: function(){
             this.$el.html(Mustache.to_html(this.template, this.model.toJSON()));
             return this;
