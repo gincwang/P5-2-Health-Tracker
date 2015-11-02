1e0778d1a4391a7f3f5b7510234abe66b44a85bf
gincwang <gincwang@gmail.com>
2015-11-01 22:58:09 -0800

    change the store view templates to take care of item quantity selection tag

--- js/views/food-store-view.js
+++ js/views/food-store-view.js
@@ -4,15 +4,56 @@ var app = app || {};
     'use strict';
 
     app.FoodStoreView = Backbone.View.extend({
+        initialize: function () {
+            this.listenTo(this.model, "change", this.render);
+            this.selectOPtions = {options: [
+                {value: 1, text: 1, selected: false},
+                {value: 2, text: 2, selected: false},
+                {value: 3, text: 3, selected: false},
+                {value: 4, text: 4, selected: false},
+                {value: 5, text: 5, selected: false},
+            ]};
+            this.selectOPtions.options[this.model.get('quantity') - 1]["selected"] = true;
+        },
+
         tagName: 'li',
 
         className: 'food-store-view',
 
-        template: '<div><p class="item-name">{{ item_name }}</p><p class="brand-name">{{ brand_name }}</p><p class="p-calories">{{ nf_calories }} Calories</p><span class="select-label">Qty</span><select class="quantity-select"><option value=1>1</option><option value=2>2</option><option value=3>3</option><option value=4>4</option><option value=5>5</option></select></div><div class="remove-btn-wrapper"><button type="button" class="remove-button">x</button></div>',
+        template: {
+            main: '<div>' +
+                    '<p class="item-name">{{ item_name }}</p>' +
+                    '<p class="brand-name">{{ brand_name }}</p>' +
+                    '<p class="p-calories">{{ nf_calories }} Calories</p>' +
+                    '<span class="select-label">Qty</span>' +
+                  '</div>' +
+                  '<div class="remove-btn-wrapper"><button type="button" class="remove-button">x</button></div>',
+            select: '<select class="quantity-select">{{#options}}' +
+                        '<option value={{value}} {{#selected}}selected{{/selected}}>' + '{{text}}' +
+                        '</option>' +
+                    '{{/options}}</select>'
+        },
 
         render: function(){
-            this.$el.html(Mustache.to_html(this.template, this.model.toJSON()));
+            this.$el.html(Mustache.to_html(this.template.main, this.model.toJSON()) + Mustache.to_html(this.template.select, this.selectOPtions));
             return this;
+        },
+
+        events: {
+            "change select": "changeQuantity"
+        },
+
+        changeQuantity: function(e) {
+            var num = $(e.target).val();
+            this.model.set({"quantity": num});
+            _.each(this.selectOPtions.options, function(element, index, list){
+                if(index === num){
+                    element.selected = true;
+                }else {
+                    element.selected = false;
+                }
+            });
+
         }
 
 
--- js/views/foodList-view.js
+++ js/views/foodList-view.js
@@ -13,27 +13,32 @@ var app = app || {};
             this.listenTo(this.collection, "add", this.render);
             this.listenTo(this.collection, "reset", this.render);
             this.listenTo(this.collection, "remove", this.render);
+            this.listenTo(this.collection, "change", this.render);
         },
 
         events: {
-            "click .remove-button": "removeFood"
+            "click .remove-button": "removeFood",
         },
 
         render: function(){
 
             this.$el.empty();
 
+            //store view and list view share the same View for food
             if(this.collection instanceof Backbone.Firebase.Collection){
                 //render each store item
                 this.collection.forEach(this.addFoodStore, this);
 
                 //render summary text
-                var calories = 0;
+                var calories = 0,
+                    totalItem = 0;
+                //get total calories and items
                 this.collection.forEach(function(element){
-                    calories += element.attributes.nf_calories * element.attributes.quantity;
+                    calories += Number(element.attributes.nf_calories * element.attributes.quantity);
+                    totalItem += Number(element.attributes.quantity);
                 });
                 var items = (this.collection.length > 1) ? "items" : "item";
-                $("#total").html("<h2 class='total-price'>Total: " + calories.toString() + " Calories (" + this.collection.length + " " + items + ")</h2>");
+                $("#total").html("<h2 class='total-price'>Total: " + calories.toString() + " Calories (" + totalItem + " " + items + ")</h2>");
             }else {
                 //render each list item
                 this.collection.forEach(this.addFood, this);
